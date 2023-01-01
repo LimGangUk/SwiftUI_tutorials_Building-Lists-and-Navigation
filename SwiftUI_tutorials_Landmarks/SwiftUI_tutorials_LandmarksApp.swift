1cba19bc71b4b036444a7717818f3284c6b9b38d
UK <gang3351@gmail.com>
2023-01-01 17:05:08 +0900

    Initial Commit

---
+++ SwiftUI_tutorials_Landmarks/SwiftUI_tutorials_LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutorials_LandmarksApp.swift
+//  SwiftUI_tutorials_Landmarks
+//
+//  Created by 임강욱 on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tutorials_LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
